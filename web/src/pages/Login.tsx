28454805ffe314c91b337f865644a621530768c8
Brijesh <softbrijeshio@gmail.com>
2025-10-27 15:17:50 +0530

    feat(web): add register and login page with working

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,3 +1,71 @@
+import { useLogin } from "@/hooks/useAuth";
+import type { LoginType } from "@/types";
+import type { FormEvent } from "react";
+import { Link } from "react-router-dom";
+
+const formFields = [
+  {
+    label: "Email Address",
+    type: "email",
+    name: "email",
+    placeholder: "Jasmine.doe@ex.com",
+  },
+  {
+    label: "Password",
+    type: "password",
+    name: "password",
+    placeholder: "*******",
+  },
+];
+
 export function Login() {
-  return <div>Login Page</div>;
+  const { mutate, isPending } = useLogin();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const data = Object.fromEntries(formData.entries());
+    mutate(data as LoginType);
+  };
+
+  return (
+    <main className="w-full h-screen flex items-center justify-center p-3 bg-base-300">
+      <section className="w-86 card bg-base-100">
+        <div className="card-body">
+          <div className="text-center">
+            <h2 className="text-3xl font-bold">Welcome back!</h2>
+            <p className="opacity-70">Log into your account and continue</p>
+          </div>
+          <form className="flex flex-col gap-3 my-5" onSubmit={handleSubmit}>
+            {formFields.map((field) => (
+              <div key={field.name}>
+                <label htmlFor={field.name}>{field.label}</label>
+                <input
+                  type={field.type}
+                  name={field.name}
+                  id={field.name}
+                  placeholder={field.placeholder}
+                  className="input"
+                  required
+                />
+              </div>
+            ))}
+            <div>
+              <button className="btn btn-primary w-full" disabled={isPending}>
+                {isPending ? "Loading..." : "Log in"}
+              </button>
+            </div>
+          </form>
+          <div className="text-center">
+            <p>
+              Don't have an account?{" "}
+              <Link to="/register" className="underline text-primary">
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,3 +1,77 @@
+import { useLogin } from "@/hooks/useAuth";
+import type { LoginType } from "@/types";
+import type { FormEvent } from "react";
+import { Link } from "react-router-dom";
+
+const formFields = [
+  {
+    label: "Full Name",
+    type: "text",
+    name: "name",
+    placeholder: "Jasmine Doe",
+  },
+  {
+    label: "Email Address",
+    type: "email",
+    name: "email",
+    placeholder: "jasmine.doe@ex.com",
+  },
+  {
+    label: "Password",
+    type: "password",
+    name: "password",
+    placeholder: "*******",
+  },
+];
+
 export function Register() {
-  return <div>Register Page</div>;
+  const { mutate, isPending } = useLogin();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const data = Object.fromEntries(formData.entries());
+    mutate(data as LoginType);
+  };
+
+  return (
+    <main className="w-full h-screen flex items-center justify-center p-3 bg-base-300">
+      <section className="w-86 card bg-base-100">
+        <div className="card-body">
+          <div className="text-center">
+            <h2 className="text-3xl font-bold">Create Account</h2>
+            <p className="opacity-70">Sign up and continue</p>
+          </div>
+          <form className="flex flex-col gap-3 my-5" onSubmit={handleSubmit}>
+            {formFields.map((field) => (
+              <div key={field.name}>
+                <label htmlFor={field.name}>{field.label}</label>
+                <input
+                  type={field.type}
+                  name={field.name}
+                  id={field.name}
+                  placeholder={field.placeholder}
+                  className="input"
+                  required
+                />
+              </div>
+            ))}
+            <div>
+              <button className="btn btn-primary w-full" disabled={isPending}>
+                {isPending ? "Loading..." : "Create Account"}
+              </button>
+            </div>
+          </form>
+          <div className="text-center">
+            <p>
+              Already have an account?{" "}
+              <Link to="/login" className="underline text-primary">
+                Log in
+              </Link>
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
