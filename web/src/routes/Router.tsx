f992bdb4dde0c5365833ebb9fe1fa1c279355a10
Brijesh <softbrijeshio@gmail.com>
2025-10-27 14:53:51 +0530

    feat(web): add routes and setup authentication

--- web/src/routes/Router.tsx
+++ web/src/routes/Router.tsx
@@ -1,16 +1,44 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Dashboard, Home, Login, Profile, Register } from "@/pages";
+import { AuthProvider } from "@/auth";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "@/auth";
+
+function RestrictedRoute() {
+  const { user, loading } = useAuth();
+
+  if (loading) return <p>Loading...</p>;
+  if (user) return <Navigate to="/c" replace />;
+
+  return <Outlet />;
+}
+
+function ProtectedRoute() {
+  const { user, loading } = useAuth();
+
+  if (loading) return <p>Loading...</p>;
+  if (!user) return <Navigate to="/login" replace />;
+
+  return <Outlet />;
+}
 
 export function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<RestrictedRoute />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/profile/:username" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
---
+++ web/src/types/index.d.ts
@@ -0,0 +1,34 @@
+export interface UserType {
+  _id: string;
+  name: string;
+  email: string;
+}
+
+export interface AuthContextType {
+  user: UserType | null;
+  isAuthenticated: boolean;
+  loading: boolean;
+}
+
+export interface RegisterType {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export type LoginType = Omit<RegisterType, "name">;
+
+export interface ChatItemProps {
+  _id: string;
+  name: string;
+  profileImage: string;
+  lastMessage: string;
+  lastSeen: string;
+  isOnline: boolean;
+}
+
+export interface MessageProps {
+  _id: string;
+  createdAt: string;
+  message: string;
+}
